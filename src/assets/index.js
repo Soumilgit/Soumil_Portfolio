bb595a9459671f28e99a560b83f662b63a763cc4
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-09-17 02:10:31 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import creator from "./creator.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import jQuery from "./tech/jQuery.png";
+import vanillajs from "./tech/vanillajs.png";
+import php from "./tech/php.png";
+import sql from "./tech/sql.png";
+
+import emfinity from "./company/emfinity.png";
+import npcil from "./company/npcil.png";
+import gssoc from "./company/gssoc.png";
+import kjsce from "./company/kjsce.png";
+import marquee from "./company/marquee.png";
+
+import boblob from "./boblob.png";
+import jasder from "./jasder.png";
+import smack from "./smack.png";
+
+export {
+  logo,
+  creator,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  jQuery,
+  vanillajs,
+  php,
+  sql,
+  emfinity,
+  npcil,
+  marquee,
+  gssoc,
+  kjsce,
+  boblob,
+  jasder,
+  smack,
+};
