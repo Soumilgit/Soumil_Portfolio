cc30020ff530fa87ec6a298a0b54ab12cfef5696
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-11-14 00:55:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,33 +15,15 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#39FF14]'>Soumil!</span>
+            Hi , I'm <span className='text-[#39FF14]'>Soumil!</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop websites,<br className='sm:block hidden' />
-            interfaces and web applications, have interest in Data Structures and Algorithms.
+            I like programming, exploring tech stuff, and nature.
           </p>
         </div>
       </div>
 
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
