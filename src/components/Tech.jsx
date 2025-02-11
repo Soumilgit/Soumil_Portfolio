abc514ce7cd75a1d36b2f5219d9fbc6921a6a851
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-11 20:45:17 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,7 +8,7 @@ const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div className='w-28 h-32' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
