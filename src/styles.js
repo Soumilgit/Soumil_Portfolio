c0bb51297f708090881f1e70903e6efe3027556b
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-12 16:23:51 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,10 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-  heroSubText:
-    "text-[#39FF14] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  "font-black text-[#ADFF2F] lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+heroSubText:
+  "text-[#ADFF2F] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+
 
   sectionHeadText:
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
