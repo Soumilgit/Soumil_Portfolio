5118b2fae4107e9c818436ca846b796f15ba09c0
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-12 16:27:58 +0530

    Add files via upload

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,22 @@
 
-import creator from "./creator.png";
+import creator from "./contentcreator.png";
 import web from "./web.png";
+import data from "./data.png";
+import problem from "./problemsolver.png";
+
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import html from "./tech/html.png";
 import css from "./tech/css.png";
 import python from "./tech/python.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
+
 import javascript from "./tech/javascript.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
 import jQuery from "./tech/jQuery.png";
 import cpp from "./tech/cpp.png";
 import php from "./tech/php.png";
@@ -28,20 +31,30 @@ import gssoc from "./company/gssoc.png";
 import meshcraft from "./company/meshcraft.png";
 import marquee from "./company/marquee.png";
 
-import boblob from "./boblob.png";
+import boblob from ".//boblob.png";
 import jasder from "./jasder.png";
 import smack from "./smack.png";
+
+import codechef from "./codechef.png";
+import cp from "./cp.png";
+import jsi from "./jsi.png";
+import oods from "./oods.png";
+import postman from "./postman.png";
+import githubk from "./githubk.png";
+
 
 export {
   creator,
   web,
+  data,
+  problem,
   github,
   menu,
   close,
+  html,
   css,
   python,
   git,
-  html,
   javascript,
   nodejs,
   reactjs,
@@ -61,5 +74,12 @@ export {
   jasder,
   smack,
   java,
-  typescript
+  typescript,
+  codechef,
+  cp,
+  jsi,
+  oods,
+  postman,
+  githubk
+
 };
