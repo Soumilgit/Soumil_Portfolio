25e14854f9dad2fb1c7c0ce9308323fd99626d2f
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-14 00:14:13 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -5,14 +5,14 @@ const styles = {
 
   heroHeadText:
   "font-black text-[#ADFF2F] lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-heroSubText:
+ heroSubText:
   "text-[#ADFF2F] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
 
   sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    "text-white font-black md:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px]",
   sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    "sm:text-[23px] text-[19px] text-secondary uppercase tracking-wider",
 };
 
 export { styles };
