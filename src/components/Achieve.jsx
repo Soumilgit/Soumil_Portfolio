c77d69390087a51668d851c206e1ba7508a37557
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-14 15:49:03 +0530

    Update Achieve.jsx

--- src/components/Achieve.jsx
+++ src/components/Achieve.jsx
@@ -16,7 +16,7 @@ const icons = [cp, codechef, githubk];
 
 const AchievementCard = ({ index, name, description }) => (
   <motion.div
-    variants={fadeIn("up", "spring", index * 0.3, 0.75)} // Scroll animation
+    variants={fadeIn("up", "spring", index * 0.2, 0.35)} // Scroll animation
     initial="hidden"
     whileInView="show"
     viewport={{ once: true, amount: 0.1 }}
