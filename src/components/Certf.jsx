b50627e8c17ffc00434aebbe0098df7bd8e4664d
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-14 17:11:36 +0530

    Update Certf.jsx

--- src/components/Certf.jsx
+++ src/components/Certf.jsx
@@ -14,7 +14,7 @@ const icons = [jsi, postman, oods];
 
 const CertificationCard = ({ index, name, description }) => (
   <motion.div
-    variants={fadeIn("up", "spring", index * 0.2, 0.35)} // Scroll animation
+    variants={fadeIn("up", "spring", index * 0.15, 0.3)} // Scroll animation
     initial="hidden"
     whileInView="show"
     viewport={{ once: true, amount: 0.1 }}
