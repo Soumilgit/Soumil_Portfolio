7d1cef0db37bd89128a1d2337ce3001ff03ef662
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-16 02:47:04 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,9 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-  "font-black text-[#ADFF2F] lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+  "font-black text-[#ADFF2F] lg:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] lg:leading-[98px] mt-2",
 heroSubText:
-  "text-[#9ACD32] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  "text-[#9ACD32] font-medium lg:text-[28px] sm:text-[28px] xs:text-[28px] text-[16px] lg:leading-[40px]",
 
 
   sectionHeadText:
