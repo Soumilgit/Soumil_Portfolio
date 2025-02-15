d4ca49554460495c5f33f3f87312191b59d69936
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-16 02:45:18 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,150 +1,109 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
-import { styles } from "../styles";
-import { DownloadCVMarquee } from "../Marquees/downloadCVMarquee";
-import { Arrow } from "../svg/arrow";
+import { motion } from "framer-motion"; 
+import { useState } from "react"; 
+import { styles } from "../styles"; 
+import { DownloadCVMarquee } from "../Marquees/downloadCVMarquee"; 
+import { Arrow } from "../svg/arrow"; 
+import { hero } from "../constants"; 
+import { SectionWrapper } from "../hoc"; 
 
-const Hero = () => {
-  const [flipped, setFlipped] = useState(false);
+const Hero = () => { 
+  const [flipped, setFlipped] = useState(false); 
+  const { title, icon } = hero[0]; 
 
-  return (
-    <section className="grid grid-cols-1 md:grid-cols-2 h-screen justify-center items-center">
-      {/* Left Side - Flippable ID Card */}
-      <div className="relative flex justify-center items-center mt-6 md:mt-0">
-        <div
-          className="relative w-72 h-[28em] sm:w-96 sm:h-[30rem] cursor-pointer perspective-1000"
-          onClick={() => setFlipped(!flipped)}
-        >
-          <motion.div
-            className="relative w-full h-full"
-            animate={{ rotateY: flipped ? 180 : 0 }}
-            transition={{ duration: 0.6 }}
-            style={{ transformStyle: "preserve-3d" }}
-          >
+  return ( 
+    <section className="grid grid-cols-1 md:grid-cols-2 h-auto justify-start items-center px-2 sm:px-2 bg-cover bg-center"> 
+      
+      {/* Left Side (ID Card) */}
+      <div className="flex justify-left items-center mt-1 md:mt-0"> 
+        <div className="relative w-72 h-[40em] sm:w-96 sm:h-[38rem] cursor-pointer perspective-1000" onClick={() => setFlipped(!flipped)}> 
+          <motion.div className="relative w-full h-full" animate={{ rotateY: flipped ? 180 : 0 }} transition={{ duration: 0.6 }} style={{ transformStyle: "preserve-3d" }}> 
+            
             {/* Front Side */}
-            <motion.div
-              className="absolute w-full h-full bg-gray-900 shadow-lg rounded-xl border border-gray-700 p-5 flex flex-col items-center justify-start text-white"
-              style={{ backfaceVisibility: "hidden" }}
-            >
-              <div className="absolute inset-0 bg-gradient-to-t from-transparent via-white/10 to-transparent rounded-xl pointer-events-none" />
-
-              {/* Tag */}
-              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-[#34d680] px-3 py-1 text-black font-bold rounded-md shadow-md">
-               Flip it!
-              </div>
-
-              {/* Image */}
-              <div className="rounded-md overflow-hidden w-full h-90 flex justify-center items-center"> 
-                <img 
-                  src="https://media.licdn.com/dms/image/v2/D4D03AQGUUIrTE5CqtA/profile-displayphoto-shrink_200_200/B4DZPQatywHMAY-/0/1734368484955?e=2147483647&v=beta&t=mN30VUV-I4VheWwmhqi9ZBajak7RS2lgz3_r4FBOL2s" 
-                  alt="My Photo" 
-                  className="w-full h-full object-contain"
-                />
-              </div>
-
-              {/* Details */}
-              <div className="mt-4 text-left w-full"> {/* Aligns text to the left */}
-                <h2 className="text-xl font-semibold text-center bg-[#393b3a]">Soumil Mukhopadhyay</h2>
-
-                <div className="text-l text-gray-300 pl-14">
-                  <p><strong>🎓Degree:</strong> Software Engineer</p>
-                  <p><strong>🌐First Passion:</strong> Web Development</p>
-                  <p><strong>📊Second Passion:</strong> Data Science</p>
-                  <p><strong>📈Third Passion:</strong> Competitive Coding</p>
-                </div>
-
-                <hr className="my-3 border-gray-600 " />
-
-                <p className="text-l text-gray-400 pl-14">📅DOB: 30 October 2004</p>
-                <p className="text-l text-gray-400 pl-14">📧Email: msoumil69@gmail.com</p>
-                <p className="text-l text-gray-400 pl-14">📞Phone: 9405727673</p>
-                <div className="mb-5"></div>
-              </div>
-              
-            </motion.div>
-
+            <motion.div className="absolute w-full h-full bg-gray-900 shadow-lg rounded-xl border border-gray-700 p-5 flex flex-col items-center justify-start text-white" style={{ backfaceVisibility: "hidden" }}> 
+              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-[#34d680] px-3 py-1 text-black font-bold rounded-md shadow-md"> 
+                Flip it! 
+              </div> 
+              <div className="rounded-md overflow-hidden w-full flex justify-center items-center neon-icon"> 
+                <img src={icon} alt={title} className="w-full h-auto max-w-[100%] md:max-w-full object-contain" /> 
+              </div> 
+              <div className="mt-4 text-left w-full"> 
+                <h2 className="text-xl font-semibold text-center bg-[#393b3a]">{title}</h2> 
+                {/* Details */}
+                <div className="mt-4 text-left w-full"> 
+                  <div className="text-l text-gray-300 pl-5 sm:pl-14"> 
+                    <p><strong>🎓 Degree:</strong> Software Engineer</p> 
+                    <p><strong>🌐 First Passion:</strong> Web Development</p> 
+                    <p><strong>📊 Second Passion:</strong> Data Science</p> 
+                    <p><strong>📈 Third Passion:</strong> Competitive Coding</p> 
+                  </div> 
+                  <hr className="my-3 border-gray-600 " /> 
+                  <p className="text-l text-gray-400 pl-5 sm:pl-14">📅 DOB: 30 October 2004</p> 
+                  <p className="text-l text-gray-400 pl-5 sm:pl-14">📧 Email: msoumil69@gmail.com</p> 
+                  <p className="text-l text-gray-400 pl-5 sm:pl-14">📞 Phone: 9405727673</p> 
+                  <div className="mb-5"></div> 
+                </div> 
+              </div> 
+            </motion.div> 
+            
             {/* Back Side */}
-            <motion.div
-  className="absolute w-full h-full bg-gray-900 text-white shadow-lg rounded-xl border border-gray-700 p-4 flex flex-col justify-between items-start overflow-hidden"
-  style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}
->
- 
-    
+            <motion.div className="absolute w-full h-full bg-gray-900 shadow-lg rounded-xl border border-gray-700 p-4 flex flex-col items-center justify-start text-white" style={{ backfaceVisibility: "hidden", transform: "rotateY(180deg)" }}> 
+              <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 bg-[#34d680] px-3 py-1 text-black font-bold rounded-md shadow-md"> 
+                Flip it! 
+              </div> 
+              <h2 className="text-lg md:text-xl font-bold mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1"> 
+                Find me online! 
+              </h2> 
+              <div className="flex flex-col items-center md:items-start w-full text-sm md:text-base space-y-1"> 
+                <a href="https://linktr.ee/FlowRM" target="_blank" className="text-blue-400 hover:underline font-semibold">🦄 My startup, FlowRM's socials</a> 
+                <a href="https://www.linkedin.com/in/soumilm30/" target="_blank" className="text-blue-400 hover:underline font-semibold">🔗 LinkedIn</a> 
+                <a href="https://github.com/Soumilgit" target="_blank" className="text-blue-400 hover:underline font-semibold">💻 GitHub</a> 
+                <a href="https://twitter.com/SoumilMukh6476/" target="_blank" className="text-blue-400 hover:underline font-semibold">🐦 Twitter</a> 
+                <a href="https://www.codechef.com/users/spry_deer_42" target="_blank" className="text-blue-400 hover:underline font-semibold">🏅 CodeChef</a> 
+              </div> 
+              <h2 className="text-lg md:text-xl font-semibold mt-4 mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1"> 
+                My Hobbies 
+              </h2> 
+              <div className="text-gray-300 flex flex-col items-center md:items-start w-full text-sm md:text-base space-y-1"> 
+                <p className="font-semibold">🦜 Birdwatching</p> 
+                <p className="font-semibold">📰 Newspaper Reading</p> 
+                <p className="font-semibold">🏓 Table Tennis</p> 
+                <p className="font-semibold">⚽ Football</p> 
+              </div> 
+              <h2 className="text-lg md:text-xl font-semibold mt-4 mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1"> 
+                My Travels 
+              </h2> 
+              <a href="https://maps.app.goo.gl/QLHqDchdPjH6bQdb8?g_st=ac" target="_blank" className="text-blue-400 hover:underline font-semibold text-sm md:text-base self-start"> 
+                📍 Google Maps 
+              </a> 
+              <div className="mb-5"></div> 
+            </motion.div> 
+          </motion.div> 
+        </div> 
+      </div> 
 
+      {/* Right Side (Text, Button, Arrow) */}
+<div className="flex flex-col items-center md:items-start gap-4 md:ml-[14.4px] md:self-center mt-8"> 
+  <h1 className={`${styles.heroHeadText} text-lime-500`}> 
+    Hi, I'm <span className="text-[#50c33c]">Soumil!</span> 
+  </h1> 
+  <p className={`${styles.heroSubText} mt-4 text-white-100 text-left`}> 
+    I like programming, exploring tech stuff, and nature. 
+  </p> 
+  <div className="mt-4"> 
+    <a href="/SoumilM_KJSCE_Resume.pdf" target="_blank" rel="noopener noreferrer"> 
+      <div className="w-32 sm:w-36 h-8 border border-[#34d680] rounded-full hover:bg-white text-[#34d680] transition duration-300 flex items-center justify-center"> 
+        <DownloadCVMarquee /> 
+      </div> 
+    </a> 
+    <div className="relative mt-10 flex items-center gap-1" style={{ transform: "rotate(14.6deg)" }}> 
+      <div style={{ transform: "rotate(180deg)" }}> 
+        <Arrow /> 
+      </div> 
+      <p className="text-[#34d680] font-semibold text-sm sm:text-xl">Gotcha, it IS a button!</p> 
+    </div> 
+  </div> 
+</div> 
+</section>
+  );}
 
-  {/* Find me online section */}
-  <h2 className="text-lg md:text-xl font-bold mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1">
-    Find me online!
-  </h2>
-  <div className="flex flex-col items-center md:items-start w-full text-sm md:text-base space-y-1">
-    <a href="https://linktr.ee/FlowRM" target="_blank" className="text-blue-400 hover:underline font-semibold">🦄 My startup, FlowRM's socials</a>
-    <a href="https://www.linkedin.com/in/soumilm30/" target="_blank" className="text-blue-400 hover:underline font-semibold">🔗 LinkedIn</a>
-    <a href="https://github.com/Soumilgit" target="_blank" className="text-blue-400 hover:underline font-semibold">💻 GitHub</a>
-    <a href="https://twitter.com/SoumilMukh6476/" target="_blank" className="text-blue-400 hover:underline font-semibold">🐦 Twitter</a>
-    <a href="https://www.codechef.com/users/spry_deer_42" target="_blank" className="text-blue-400 hover:underline font-semibold">🏅 CodeChef</a>
-  </div>
-
-  {/* Hobbies Section */}
-  <h2 className="text-lg md:text-xl font-semibold mt-4 mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1">
-    My Hobbies
-  </h2>
-  <div className="text-gray-300 flex flex-col items-center md:items-start w-full text-sm md:text-base space-y-1">
-    <p className="font-semibold">🦜 Birdwatching</p>
-    <p className="font-semibold">📰 Newspaper Reading</p>
-    <p className="font-semibold">🏓 Table Tennis</p>
-    <p className="font-semibold">⚽ Football</p>
-  </div>
-
-  {/* Google Maps Profile */}
-  <h2 className="text-lg md:text-xl font-semibold mt-4 mb-3 self-center w-full text-center border-b-2 border-gray-300 pb-1">
-    My Travels
-  </h2>
-  <a 
-    href="https://maps.app.goo.gl/QLHqDchdPjH6bQdb8?g_st=ac" 
-    target="_blank" 
-    className="text-blue-400 hover:underline block font-semibold text-sm md:text-base"
-  >📍 Google Maps</a>
-   <div className="mb-5"></div>
-  
-
-
- 
-</motion.div>
-
-
-
-          </motion.div>
-        </div>
-      </div>
-
-<div className="flex flex-col items-center md:items-start gap-4 md:ml-[-88px] md:self-center">
-  <h1 className={`${styles.heroHeadText} text-lime-500`}>
-    Hi, I'm <span className="text-[#50c33c]">Soumil!</span>
-  </h1>
-  <p className={`${styles.heroSubText} mt-10 text-white-100`}>
-    I like programming, exploring tech stuff, and nature.
-  </p>
-
-  {/* Download Resume Button */}
-  <div className="mt-4">
-    <a href="/SoumilM_KJSCE_Resume.pdf" target="_blank" rel="noopener noreferrer">
-      <div className="w-32 sm:w-36 h-8 border border-[#34d680] rounded-full hover:bg-white text-[#34d680] transition duration-300 flex items-center justify-center">
-        <DownloadCVMarquee />
-      </div>
-    </a>
-
-    {/* Arrow and Gotcha Text */}
-    <div className="relative mt-10 flex items-center gap-1 md:ml-[-6px]" style={{ transform: "rotate(15.26deg)" }}>
-      <div style={{ transform: "rotate(180deg)" }}>
-        <Arrow />
-      </div>
-      <p className="text-[#34d680] font-semibold text-sm sm:text-xl">Gotcha, it IS a button!</p>
-    </div>
-  </div>
-</div>
-
-    </section>
-  );
-};
-
-export default Hero;
+export default SectionWrapper(Hero, "hero");
