83b4ac4fd5ddabc8c2e3b527f66e39bd7e4cbffe
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-25 22:57:35 +0530

    ignore this gap alignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
     </Link>
 
     {/* Desktop Navbar */}
-    <ul className="nav-links ml-12" style={{ marginRight: "-93.05px" }}> 
+    <ul className="nav-links ml-12" style={{ marginRight: "-92.46px" }}> 
       {navLinks.map((nav) => (
         <motion.li
           key={nav.id}
