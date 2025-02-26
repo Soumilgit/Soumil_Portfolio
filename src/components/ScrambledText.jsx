9e1565b0b906649e9b9b3a19b808f1beaf1c72ba
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-27 04:50:35 +0530

    Update ScrambledText.jsx

--- src/components/ScrambledText.jsx
+++ src/components/ScrambledText.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export const ScrambledText = ({ text, duration = 1730 }) => {
+export const ScrambledText = ({ text, duration = 1440 }) => {
   const [scrambledText, setScrambledText] = useState(text);
   const [isLoaded, setIsLoaded] = useState(false);
 
