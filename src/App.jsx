cc7e1ebd6f2857b94c9d2678152e7255b7301c37
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-02-26 19:26:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
-import { Hero, About, Contact, Experience, Feedbacks, Navbar, Tech,Achieve, Works,Certf, StarsCanvas } from "./components";
+import { Hero, About, Contact, Experience, Feedbacks, Navbar, Tech, Achieve, Works, Certf, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-      <div
-  className="bg-hero-pattern bg-cover bg-fixed bg-no-repeat bg-center"
-  style={{
-    
-    backgroundBlendMode: "overlay",
-    backgroundColor: "rgba(255, 255, 255,0)",
-  }}
->
-  <Navbar />
-  <Hero />
-</div>
-        <About />
-        <Experience />
-        <Tech />
-        <Achieve />
-        <Works />
-        <Certf />
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
+    <>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div
+            className="bg-hero-pattern bg-cover bg-fixed bg-no-repeat bg-center"
+            style={{
+              backgroundBlendMode: "overlay",
+              backgroundColor: "rgba(255, 255, 255,0)",
+            }}
+          >
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Achieve />
+          <Works />
+          <Certf />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
