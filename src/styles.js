9eec755325faca47dae7410db2c1a7328520a9dc
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-01 13:43:55 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -4,13 +4,17 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-  "font-black text-[#90ef91] lg:text-[48px] sm:text-[46px] xs:text-[45px] text-[40px] lg:leading-[88px] mt-2",
-  heroSubText:
-  "text-[#ccfd7f] font-medium lg:text-[25px] sm:text-[23px] xs:text-[21px] text-[21px] lg:leading-[20px]",
+    "font-black text-[#90ef91] lg:text-[46px] sm:text-[42px] xs:text-[35px] text-[34px] lg:leading-[88px] mt-2",
+  
+  // New style for mobile font size
+  heroHeadTextMobile: "text-[29px] sm:text-[30px] lg:text-[38px] font-black text-[#90ef91] lg:leading-[88px] mt-2",
 
+  heroSubText:
+    "text-[#ccfd7f] font-medium lg:text-[27px] sm:text-[25px] xs:text-[21px] text-[21px] lg:leading-[20px]",
 
   sectionHeadText:
     "text-white font-black md:text-[65px] sm:text-[55px] xs:text-[45px] text-[35px]",
+  
   sectionSubText:
     "sm:text-[25px] text-[22px] text-secondary uppercase tracking-wider",
 };
