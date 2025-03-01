65e7e164b79a0a977a047d4561ee3c0a7c989855
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-01 19:00:44 +0530

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -6,8 +6,7 @@ const styles = {
   heroHeadText:
     "font-black text-[#90ef91] lg:text-[43px] sm:text-[42px] xs:text-[35px] text-[34px] lg:leading-[88px] mt-2",
   
-  // New style for mobile font size
-  heroHeadTextMobile: "text-[27.9px] sm:text-[28.5px] sm:text-[34px] font-black text-[#90ef91] lg:leading-[88px] mt-2",
+  heroHeadTextMobile: "text-[29.7px] sm:text-[29.4px] sm:text-[34px] font-black text-[#90ef91] lg:leading-[88px] mt-2",
 
   heroSubText:
     "text-[#ccfd7f] font-medium lg:text-[27px] sm:text-[25px] xs:text-[21px] text-[21px] lg:leading-[20px]",
@@ -17,6 +16,9 @@ const styles = {
   
   sectionSubText:
     "sm:text-[25px] text-[22px] text-secondary uppercase tracking-wider",
+
+  // New style for ScrambledText
+  //scrambledText: "inline-block max-w-full whitespace-nowrap overflow-visible text-ellipsis px-1 box-border",
 };
 
 export { styles };
