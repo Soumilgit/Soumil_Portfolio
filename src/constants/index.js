ac93ed451f9442d8613d2b302b6593f156c682bf
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-02 12:19:24 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -270,7 +270,7 @@ const achievements = [
     
   },
   {
-    name: "Over 2000+ contributions on GitHub",
+    name: "Over 2500+ contributions on GitHub",
     description:
       "Have been an active member of GitHub community and have contributed and worked on 40+ projects.",
   },
