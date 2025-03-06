114cb062155547f51185435208d40de447859b55
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-06 20:45:03 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
     </Link>
 
     {/* Desktop Navbar */}
-    <ul className="nav-links ml-12" style={{ marginRight: "-78.3px" }}> 
+    <ul className="nav-links ml-12" style={{ marginRight: "-79.2px" }}> 
       {navLinks.map((nav) => (
         <motion.li
           key={nav.id}
