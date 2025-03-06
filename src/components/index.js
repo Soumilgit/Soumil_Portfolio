00c82aa8ff278fc140b6a6054068cf36a1e9a547
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-06 20:32:38 +0530

    n

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
