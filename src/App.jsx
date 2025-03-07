927c57f13d7c95a9ddd717b047c8f2bf19b37547
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2025-03-07 15:02:49 +0530

    added the starry background to all components except Hero and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"; //Displays web analytics once deployed!
+import { Analytics } from "@vercel/analytics/react"; // Displays web analytics once deployed!
 
-import { Hero, About, Contact, Experience, Feedbacks, Navbar, Tech, Achieve, Works, Certf, StarsCanvas } from "./components";
+import {
+  Hero,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Navbar,
+  Tech,
+  Achieve,
+  Works,
+  Certf,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
@@ -9,7 +21,7 @@ const App = () => {
       <BrowserRouter>
         <div className='relative z-0 bg-primary'>
           <div
-            className="bg-hero-pattern bg-cover bg-fixed bg-no-repeat bg-center"
+            className='bg-hero-pattern bg-cover bg-fixed bg-no-repeat bg-center'
             style={{
               backgroundBlendMode: "overlay",
               backgroundColor: "rgba(255, 255, 255,0)",
@@ -18,22 +30,25 @@ const App = () => {
             <Navbar />
             <Hero />
           </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Achieve />
-          <Works />
-          <Certf />
-          <Feedbacks />
-          <div className='relative z-0'>
-            <Contact />
+          <div className='relative z-2'>
             <StarsCanvas />
+            <div className='relative z-10'>
+              <About />
+              <Experience />
+              <Tech />
+              <Achieve />
+              <Works />
+              <Certf />
+              <Feedbacks />
+              <Contact />
+            </div>
           </div>
         </div>
       </BrowserRouter>
       <Analytics />
     </>
   );
-}
-//Didn't add custom scrollbar because of expected non-responsiveness of it.
-export default App; 
+};
+
+// Didn't add custom scrollbar because of expected non-responsiveness of it.
+export default App;
